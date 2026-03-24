f9fd7ac1c5b51bd3e2b93089e89c082de88158c7
demo <fake123@gmial.com>
2026-03-24 13:13:56 +0530

    Change Localhost to production

--- backend/src/main.js
+++ backend/src/main.js
@@ -17,8 +17,9 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  // origin: "https://mern-ecommerce-frontend-0jer.onrender.com",
-   origin: "http://localhost:5173", credentials: true
+  origin: "https://mern-ecommerce-frontend-0jer.onrender.com",
+  //  origin: "http://localhost:5173"
+   credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
