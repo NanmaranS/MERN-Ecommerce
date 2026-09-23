d13cefff6cdef4a5efeef59ae34c4d883f8b14f4
Nanmaran S <nanmaran.s13@gmail.com>
2026-09-23 13:40:33 +0530

    Fix production URLs and CORS

--- backend/src/main.js
+++ backend/src/main.js
@@ -11,7 +11,7 @@ import cartRouter from "./routers/cartRouter.js";
 import registerRouter from "./routers/registerRouter.js";
 import LoginRouter from "./routers/loginRouter.js";
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -9,8 +9,8 @@ export default function Cart() {
   const [cart, setCart] = useState([])
   const nav = useNavigate()
 
-  const BASE_URL = 'http://localhost:5000'
-  // const BASE_URL = "https://mern-ecommerce-back-j8ux.onrender.com" 
+  // const BASE_URL = 'http://localhost:5000'
+   const BASE_URL = "https://mern-ecommerce-back-j8ux.onrender.com" 
 
   useEffect(() => {
 
