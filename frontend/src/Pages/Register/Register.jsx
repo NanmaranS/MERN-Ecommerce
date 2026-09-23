d023225b3a31c4a5e34568611b109a6e26e22066
Nanmaran S <nanmaran.s13@gmail.com>
2026-09-23 13:57:56 +0530

    Remove sensitive registration log

--- frontend/src/Pages/Register/Register.jsx
+++ frontend/src/Pages/Register/Register.jsx
@@ -23,7 +23,6 @@ export default function Register() {
       password:Password
     })
     .then((res)=>{
-      console.log(res.data);
       setUsername('')
       setPassword('')
       setError("")
